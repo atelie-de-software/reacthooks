ec2455ac8758ad53341dec87abea325218ae99b9
davimattos <davimattosdev@gmail.com>
2019-08-18 17:34:21 -0300

    adding new technology dynamically

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 
 function App() {
   const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState("");
 
   function handleAdd() {
-    setTechs([...techs, "New Tech"]);
+    setTechs([...techs, newTech]);
+    setNewTech("");
   }
 
   return (
     <div>
       <ul>{techs && techs.map(tech => <li>{tech}</li>)}</ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
       <button type="button" onClick={handleAdd}>
         Add New Tech
       </button>
