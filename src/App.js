cfa31bd874dea4e0fcb686412c967efd8a803ad5
davimattos <davimattosdev@gmail.com>
2019-08-18 17:26:29 -0300

    created a new function to add new tech in the array

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import React, { useState } from "react";
 function App() {
   const [techs, setTechs] = useState([]);
 
+  function handleAdd() {
+    setTechs([...techs, "New Tech"]);
+  }
+
   return (
     <div>
       <ul>{techs && techs.map(tech => <li>{tech}</li>)}</ul>
+      <button type="button" onClick={handleAdd}>
+        Add New Tech
+      </button>
     </div>
   );
 }
