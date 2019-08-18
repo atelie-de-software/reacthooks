ad119b6504072b3a7583669f1cb6e0fcee772588
davimattos <davimattosdev@gmail.com>
2019-08-18 17:36:51 -0300

    creating a focus reference between button and field

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function App() {
   const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState("");
+  const inputElement = useRef(null);
 
   function handleAdd() {
     setTechs([...techs, newTech]);
     setNewTech("");
+    inputElement.current.focus();
   }
 
   return (
     <div>
       <ul>{techs && techs.map(tech => <li>{tech}</li>)}</ul>
-      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <input
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+        ref={inputElement}
+      />
       <button type="button" onClick={handleAdd}>
         Add New Tech
       </button>
