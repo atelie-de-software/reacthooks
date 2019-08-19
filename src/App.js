e5c166f420f47e6b7773d4dc5557772d7cb1796e
davimattos <davimattosdev@gmail.com>
2019-08-18 23:17:48 -0300

    useEffect simulating didUpdate and didMount

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 function App() {
-  const [techs, setTechs] = useState([]);
-  const [newTech, setNewTech] = useState("");
+  const [techs, setTechs] = useState([]); // <-declaring variables using useState
+  const [newTech, setNewTech] = useState(""); // <-declaring variables using useState
   const inputElement = useRef(null);
 
   function handleAdd() {
-    setTechs([...techs, newTech]);
-    setNewTech("");
+    setTechs([...techs, newTech]); // <-similar to setState
+    setNewTech(""); // <-similar to setState
     inputElement.current.focus();
   }
 
+  //simulates the componentDidMount when no dependency is passed
+  useEffect(() => {
+    const storageTech = localStorage.getItem("techs");
+    if (storageTech) setTechs(JSON.parse(storageTech));
+    console.log("mounted");
+  }, []); // <-dependency
+
+  //simulates the componentDidUpdate when a dependency is passed
+  useEffect(() => {
+    localStorage.setItem("techs", JSON.stringify(techs));
+    console.log("updated");
+  }, [techs]); // <-dependency
+
   return (
     <div>
-      <ul>{techs && techs.map(tech => <li>{tech}</li>)}</ul>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
       <input
         value={newTech}
         onChange={e => setNewTech(e.target.value)}
